6563a3900bb14f5484219e1626c09df905a52d5f
Amarendhar <ambl24@student.bth.se>
2025-05-03 20:16:19 +0200

    Update application2.py

--- application2.py
+++ application2.py
@@ -1,18 +1,18 @@
-import Flask
+import flask
 import time
 import socket
 
 
 host_name = socket.gethostname()
-ip_address = socket.gethostbyname(host_name)
+IP_address = socket.gethostbyname(host_name)
 
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    current_time = time.strftime("%H:%M:%S")
-    return f"{current_time} - Serving from {"host_name"} "({ip_address})\n"
+    Time = time.strftime("%H:%M:%S")
+    return Time+" Serving from "+host_name+" ("+IP_address+")\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
